77b04db0e06ecbd751ebc9591e335a74ef9118bb
Jackson Trader <jackson.trader.dev@gmail.com>
2025-11-12 23:57:57 -0600

    Update DynamicProgramming01.java

--- src/DynamicProgramming01.java
+++ src/DynamicProgramming01.java
@@ -1,40 +1,47 @@
 import java.util.*;
 
 class DynamicProgramming01 {
-    static int knapsack(int n, int m, int[] weights, int[] values) {
-        // Initialize 2D dp array (Already initialized to 0 by Java)
-        int[][] dp = new int[n + 1][m + 1];
 
-        for (int i = 1; i <= n; i++) { // Gradually increase num of items considered
-            for (int w = 1; w <= m; w++) { // Gradually increase capacity
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt(); // number of items
+        int c = scanner.nextInt(); // knapsack capacity
+        int[] weights = new int[n];
+        int[] values = new int[n];
+
+        // Read weight and value for each item
+        for (int i = 0; i < n; i++) {
+            weights[i] = scanner.nextInt();
+            values[i] = scanner.nextInt();
+        }
+
+        System.out.println(knapsack(n, c, weights, values));
+    }
+
+    static int knapsack(int n, int c, int[] weights, int[] values) {
+        int[][] dp = new int[n + 1][c + 1];
+
+        for (int i = 1; i <= n; i++) { // i = how many items considered
+            for (int w = 1; w <= c; w++) { // w = current capacity
+                int currentItem = i - 1; // to make indexing easier
+
                 // If current item fits in knapsack decide to grab or skip it
-                if (weights[i] <= w) {
+                if (weights[currentItem] <= w) {
+                    int remaining = w - weights[currentItem]; // remaining capacity
+
+                    // Previous row (don't take item)
                     int skipItem = dp[i - 1][w];
-                    int grabItem = dp[i - 1][w - weights[i]] + values[i];
+                    // Previous row at remaining capacity + value of current item
+                    int grabItem = dp[i - 1][remaining] + values[currentItem];
+
                     dp[i][w] = Math.max(skipItem, grabItem);
                 } else {
-                    // Can't include item in bag because too much weight, just copy from row above (i.e. Don't add item)
-                    dp[i][w] = dp[i - 1][w];
+                    dp[i][w] = dp[i - 1][w]; // just copy from previous row
                 }
             }
         }
 
-        return dp[n][m];
-    }
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt(); // Num of items
-        int m = in.nextInt(); // Weight capacity
-        int[] weights = new int[n + 1]; // Weights of each item
-        int[] values = new int[n + 1]; // Values of each item
-
-        // Obtain weight and value of each item x_i
-        for (int i = 1; i <= n; i++) {
-            weights[i] = in.nextInt();
-            values[i] = in.nextInt();
-        }
-
-        System.out.println(knapsack(n, m, weights, values));
+        return dp[n][c];
     }
 }
